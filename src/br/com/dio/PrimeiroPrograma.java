1055dd318da5a080f220cb6ca2476d29235a2d51
tiagocabralk <tiagocabralk@outlook.com>
2022-02-15 00:44:43 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        /*String nome = "João";
+
+        if (nome == "Tiago") {
+            System.out.print("Olá " + nome + ", bem vindo ao mundo Java!");
+        } else {
+            System.out.print("Ainda não fomos apresentados!");
+        }*/
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+    }
+}
